47e456b9ea8cf47119fae3662a14208d236f95fc
sonjoy <sonjoywc@gmail.com>
2021-09-07 02:31:50 +0600

    graph show

--- src/Components/Upload.js
+++ src/Components/Upload.js
@@ -1,14 +1,22 @@
 import React, { Fragment, useState } from 'react'
-import { Button, Row, Col, Form, FormGroup, Label, Input, Table } from 'reactstrap';
+import { Button, Row, Col, Form, FormGroup, Label, Input, Table, TabContent, TabPane, Nav, NavItem, NavLink, Card, CardTitle, CardText } from 'reactstrap';
 import { CsvToHtmlTable } from 'react-csv-to-table';
 
 import { useSelector, useDispatch } from 'react-redux'
+import classnames from 'classnames';
+
+import Plot from 'react-plotly.js';
 
 function Upload() {
     const [fileCsv, setFileCsv] = useState()
     const [csvValue, setCsvValue] = useState('')
     const [headers, setHeader] = useState([])
     const [rows, setRows] = useState([])
+    const [activeTab, setActiveTab] = useState('1');
+
+    const toggle = tab => {
+        if (activeTab !== tab) setActiveTab(tab);
+    }
     const onFileChange = e => {
         console.log('upload', e.target.files[0])
         setFileCsv(e.target.files[0])
@@ -51,31 +59,38 @@ function Upload() {
             mainrow.push(output)
         })
 
-        console.log('export',mainrow)
+        console.log('export', mainrow)
     }
     return (
         <Fragment>
+            <div className="csv_upload">
             <Form inline>
                 <Row form>
-                    <Col md={6}>
+                    <Col md={3}>
                         <FormGroup>
-                            <Label for="exampleEmail">Upload</Label>
-                            <Input type="file" onChange={onFileChange} name="email" id="exampleEmail" placeholder="with a placeholder" />
+                            <Label for="exampleEmail">CSV File Link:</Label>
                         </FormGroup>
+                    </Col>
+                    <Col md={9}>
+                        <FormGroup>                            
+                                <Input type="file" onChange={onFileChange} name="email" id="exampleEmail" placeholder="with a placeholder" />
+                                <Button color="info" onClick={upload}>Submit</Button>
+                            </FormGroup>
+                            
                     </Col>
 
                 </Row>
 
-                <Button color="info" onClick={upload}>Submit</Button>
+                
             </Form>
-
+            </div>
             {/* {
                 Object.keys(csvValue).length !== 0 && <CsvToHtmlTable
                     data={fileCsv}
                     csvDelimiter=","
                 />
             } */}
-            <button className="btn btn-primary" onClick={exportCsv}>Export to csv</button>
+            {/* <button className="btn btn-primary" onClick={exportCsv}>Export to csv</button> */}
             <Table bordered>
                 <thead>
                     <tr>
@@ -90,16 +105,111 @@ function Upload() {
                     {
                         rows.length > 0 && rows.map(res => {
                             let row = res.split(',')
-                            console.log('sonjoy',row)
+                            console.log('sonjoy', row)
                             return (<tr>
                                 {row.length > 0 && row.map(data => {
-                                   return <td>{data}</td>
+                                    return <td>{data}</td>
                                 })}
                             </tr>)
                         })
                     }
                 </tbody>
             </Table>
+
+
+            <div className="graph">
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: activeTab === '1' })}
+                            onClick={() => { toggle('1'); }}
+                        >
+                            Scatter
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: activeTab === '2' })}
+                            onClick={() => { toggle('2'); }}
+                        >
+                            Box
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: activeTab === '3' })}
+                            onClick={() => { toggle('1'); }}
+                        >
+                            Histogram
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+
+                <TabContent activeTab={activeTab}>
+                    <TabPane tabId="1">
+                        <Row>
+                            <Col sm="12">
+                                <Plot
+                                    data={[
+                                        {
+                                            x: [1, 2, 3],
+                                            y: [2, 6, 3],
+                                            type: 'scatter',
+                                            mode: 'lines+markers',
+                                            marker: { color: 'red' },
+                                        },
+                                        { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
+                                    ]}
+                                    layout={{ width: 320, height: 240, title: 'A Scatter Plot' }}
+                                />
+                            </Col>
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId="2">
+                        <Row>
+                            <Col sm="12">
+                                <Plot
+                                    data={[
+                                        {
+                                            x: [1, 2, 3],
+                                            y: [2, 6, 3],
+                                            type: 'box',
+                                            mode: 'lines+markers',
+                                            marker: { color: 'red' },
+                                        },
+                                        { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
+                                    ]}
+                                    layout={{ width: 320, height: 240, title: 'A Box Plot' }}
+                                />
+                            </Col>
+
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId="3">
+                        <Row>
+                            <Col sm="12">
+                                <Plot
+                                    data={[
+                                        {
+                                            x: [1, 2, 3],
+                                            y: [2, 6, 3],
+                                            type: 'histogram',
+                                            mode: 'lines+markers',
+                                            marker: { color: 'red' },
+                                        },
+                                        { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
+                                    ]}
+                                    layout={{ width: 320, height: 240, title: 'A Histogram Plot' }}
+                                />
+                            </Col>
+
+                        </Row>
+                    </TabPane>
+                </TabContent>
+
+            </div>
+
+
         </Fragment>
 
     )
